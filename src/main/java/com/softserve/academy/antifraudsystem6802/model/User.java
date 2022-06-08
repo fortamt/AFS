1cdcfe7f7c99084c01e3dbd02f002e589e9980df
Ilya Kosarenko <kosarenko2020kb13@student.karazin.ua>
2022-06-08 16:55:02 +0300

    Added field Role to User and getter

--- src/main/java/com/softserve/academy/antifraudsystem6802/model/User.java
+++ src/main/java/com/softserve/academy/antifraudsystem6802/model/User.java
@@ -3,10 +3,13 @@ package com.softserve.academy.antifraudsystem6802.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Getter
@@ -25,4 +28,11 @@ public class User implements UserDetails, UserDetailsMixin {
     @NotEmpty
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     String password;
+
+    Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(role);
+    }
 }
