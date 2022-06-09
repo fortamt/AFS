bd41476aaf1c5feee67bdb25fc6c5a80bc879743
fortamt <fortamt@gmail.com>
2022-06-09 12:44:12 +0300

    ADF-14 IpRepository -> created for managing ip`s, with helpful methods Ip -> Entity class SecurityConfig -> added authentication endpoint "suspicious-ip" TransactionController -> added SaveSuspiciousIp method TransactionService -> added instance of IpRepository                       constructor                       addSuspiciousIp(Ip ip) method

--- src/main/java/com/softserve/academy/antifraudsystem6802/controller/TransactionController.java
+++ src/main/java/com/softserve/academy/antifraudsystem6802/controller/TransactionController.java
@@ -1,16 +1,17 @@
 package com.softserve.academy.antifraudsystem6802.controller;
 
+import com.softserve.academy.antifraudsystem6802.model.Ip;
 import com.softserve.academy.antifraudsystem6802.model.Result;
 import com.softserve.academy.antifraudsystem6802.model.request.TransactionRequest;
 import com.softserve.academy.antifraudsystem6802.model.response.TransactionResultResponse;
 import com.softserve.academy.antifraudsystem6802.service.TransactionService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
+import java.util.regex.Pattern;
 
 @RestController
 @RequestMapping("/api/antifraud")
@@ -23,4 +24,12 @@ public class TransactionController {
         Result result = transactionService.process(request.getAmount());
         return new TransactionResultResponse(result);
     }
+
+    @PostMapping("/suspicious-ip")
+    @ResponseStatus(HttpStatus.CREATED)
+    Ip saveSuspiciousIp(@Valid @RequestBody Ip ip){
+        return transactionService.addSuspiciousIp(ip)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.CONFLICT));
+    }
+
 }
--- src/main/java/com/softserve/academy/antifraudsystem6802/service/TransactionService.java
+++ src/main/java/com/softserve/academy/antifraudsystem6802/service/TransactionService.java
@@ -1,10 +1,19 @@
 package com.softserve.academy.antifraudsystem6802.service;
 
+import com.softserve.academy.antifraudsystem6802.model.Ip;
 import com.softserve.academy.antifraudsystem6802.model.Result;
+import com.softserve.academy.antifraudsystem6802.repository.IpRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class TransactionService {
+    IpRepository ipRepository;
+
+    public TransactionService(IpRepository ipRepository) {
+        this.ipRepository = ipRepository;
+    }
 
     public Result process(long amount) {
         if (amount <= 200) {
@@ -14,5 +23,13 @@ public class TransactionService {
         } else {
             return Result.PROHIBITED;
         }
+    }
+
+
+    public Optional<Ip> addSuspiciousIp(Ip ip) {
+        if(ipRepository.existsByIpAddressIgnoreCase(ip.getIpAddress())){
+            return Optional.empty();
+        }
+        return Optional.of(ipRepository.save(ip));
     }
 }
