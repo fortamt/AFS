c1811ae64700836e61c0e703ca63a4e309aad68a
Ilya Kosarenko <kosarenko2020kb13@student.karazin.ua>
2022-06-09 13:45:45 +0300

    Added implementation of stolen cards + validator

--- src/main/java/com/softserve/academy/antifraudsystem6802/controller/TransactionController.java
+++ src/main/java/com/softserve/academy/antifraudsystem6802/controller/TransactionController.java
@@ -1,16 +1,17 @@
 package com.softserve.academy.antifraudsystem6802.controller;
 
 import com.softserve.academy.antifraudsystem6802.model.Result;
+import com.softserve.academy.antifraudsystem6802.model.StolenCard;
 import com.softserve.academy.antifraudsystem6802.model.request.TransactionRequest;
 import com.softserve.academy.antifraudsystem6802.model.response.TransactionResultResponse;
 import com.softserve.academy.antifraudsystem6802.service.TransactionService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/antifraud")
@@ -22,5 +23,23 @@ public class TransactionController {
     TransactionResultResponse transactionPost(@Valid @RequestBody TransactionRequest request) {
         Result result = transactionService.process(request.getAmount());
         return new TransactionResultResponse(result);
+    }
+
+    @PostMapping("/stolencard")
+    @ResponseStatus(HttpStatus.CREATED)
+    StolenCard addStolenCard(@Valid @RequestBody StolenCard stolenCard) {
+        return transactionService.addStolenCard(stolenCard);
+    }
+
+    @DeleteMapping("/stolencard/{number}")
+    @ResponseStatus(HttpStatus.OK)
+    Map<String, String> deleteStolenCard(@PathVariable String number) {
+        return transactionService.deleteStolenCard(number);
+    }
+
+    @GetMapping("/stolencard")
+    @ResponseStatus(HttpStatus.OK)
+    List<StolenCard> listStolenCards() {
+        return transactionService.listStolenCards();
     }
 }
--- src/main/java/com/softserve/academy/antifraudsystem6802/service/TransactionService.java
+++ src/main/java/com/softserve/academy/antifraudsystem6802/service/TransactionService.java
@@ -1,10 +1,24 @@
 package com.softserve.academy.antifraudsystem6802.service;
 
 import com.softserve.academy.antifraudsystem6802.model.Result;
+import com.softserve.academy.antifraudsystem6802.model.StolenCard;
+import com.softserve.academy.antifraudsystem6802.model.User;
+import com.softserve.academy.antifraudsystem6802.repository.StolenCardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class TransactionService {
+
+    @Autowired
+    StolenCardRepository stolenCardRepository;
 
     public Result process(long amount) {
         if (amount <= 200) {
@@ -14,5 +28,36 @@ public class TransactionService {
         } else {
             return Result.PROHIBITED;
         }
+    }
+
+    @Transactional
+    public StolenCard addStolenCard(StolenCard stolenCard) {
+        if(stolenCardRepository.existsByNumber(stolenCard.getNumber())) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT);
+        } else {
+            stolenCardRepository.save(stolenCard);
+            return stolenCard;
+        }
+    }
+
+    @Transactional
+    public Map<String, String> deleteStolenCard(String number) {
+        if(number.length() != 16) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+        if(!stolenCardRepository.existsByNumber(number)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        } else {
+            StolenCard stolenCard = stolenCardRepository.findByNumber(number);
+            stolenCardRepository.delete(stolenCard);
+            return Map.of("status", "Card " + stolenCard.getNumber() + " successfully removed!");
+        }
+    }
+
+    @Transactional
+    public List<StolenCard> listStolenCards() {
+        return stolenCardRepository.findAll(
+                Sort.sort(StolenCard.class).by(StolenCard::getId).ascending()
+        );
     }
 }
