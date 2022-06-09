3e9b641c21638aca6305e20714a267bea0a7aa3d
fortamt <fortamt@gmail.com>
2022-06-09 20:44:08 +0300

    minor fix

--- src/main/java/com/softserve/academy/antifraudsystem6802/repository/IpRepository.java
+++ src/main/java/com/softserve/academy/antifraudsystem6802/repository/IpRepository.java
@@ -6,11 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface IpRepository extends JpaRepository<Ip, Long> {
-
     Optional<Ip> findByIpAddressIgnoreCase(String ip);
-
     boolean existsByIpAddressIgnoreCase(String ip);
-
     int deleteByIpAddressIgnoreCase(String ip);
 
 }
